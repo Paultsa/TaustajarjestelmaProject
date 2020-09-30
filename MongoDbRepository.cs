b9d0b4b2fdd470a85d92cd352f7af6726e29faa0
Paultsa <pauli.alasaari@gmail.com>
2020-09-30 12:18:24 +0300

    mongodb

--- MongoDbRepository.cs
+++ MongoDbRepository.cs
@@ -5,17 +5,44 @@ using MongoDB.Bson;
 using System.Linq;
 using System.Collections.Generic;
 
-
-public class MongoDbRepository : IRepository
+namespace Project
 {
-    private readonly IMongoCollection<Player> _playerCollection;
-    private readonly IMongoCollection<BsonDocument> _bsonDocumentCollection;
-
-    public MongoDbRepository()
+    public class MongoDbRepository : IRepository
     {
-        var mongoClient = new MongoClient("mongodb://localhost:27017");
-        var database = mongoClient.GetDatabase("game");
-        _playerCollection = database.GetCollection<Player>("players");
-        _bsonDocumentCollection = database.GetCollection<BsonDocument>("players");
+        private readonly IMongoCollection<Map> _mapCollection;
+
+        private readonly IMongoCollection<BsonDocument> _bsonDocumentCollection;
+
+        public MongoDbRepository()
+        {
+            var mongoClient = new MongoClient("mongodb://localhost:27017");
+            var database = mongoClient.GetDatabase("game");
+            _mapCollection = database.GetCollection<Map>("maps");
+            _bsonDocumentCollection = database.GetCollection<BsonDocument>("players");
+        }
+        public async Task<Map> CreateMap(int xSize, int ySize, string name)
+        {
+            Map map = new Map()
+            {
+                tiles = new List<List<MapTile>>(),
+                id = name
+            };
+            for (int x = 0; x < xSize; x++)
+            {
+                for (int y = 0; y < ySize; y++)
+                {
+                    map.tiles[x][y] = new MapTile();
+                }
+            }
+            await _mapCollection.InsertOneAsync(map);
+            return map;
+        }
+        public async Task<Player> CreatePlayer(string mapId, Player player)
+        {
+            var filter = Builders<Map>.Filter.Eq(m => m.id, mapId);
+            await _mapCollection.FindOneAndUpdateAsync(scoreFilter, update);
+        }
+
     }
+
 }
