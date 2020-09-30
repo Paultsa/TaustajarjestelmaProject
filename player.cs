05e50837ec1e5f946401fcfef237d586bd4a1b0e
Dartti88 <41620991+Dartti88@users.noreply.github.com>
2020-09-30 10:40:33 +0300

    player3

--- player.cs
+++ player.cs
@@ -1,22 +1,17 @@
-namespace GameWebApi
+namespace Project
 {
     public class Player : ICharacter
     {
         public string Id { get; set; }
         public string Name { get; set; }
-
         public int Damage { get; set; }
         public int Score { get; set; }
         public int Level { get; set; }
-
         public int X { get; set; }
-
         public int Y { get; set; }
         public DateTime CreationTime { get; set; }
 
         public List<Item> List_items = new List<Item>();
-
-        //public string[] Tags;
 
         public static implicit operator Task<object>(Player v)
         {
