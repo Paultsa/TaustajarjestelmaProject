97b132156a4e200cc0e5baa0349239aeaf9bbf82
Paultsa <pauli.alasaari@gmail.com>
2020-09-30 12:54:27 +0300

    CreatePlayer

--- MongoDbRepository.cs
+++ MongoDbRepository.cs
@@ -20,16 +20,22 @@ namespace Project
             _mapCollection = database.GetCollection<Map>("maps");
             _bsonDocumentCollection = database.GetCollection<BsonDocument>("players");
         }
-        public async Task<Map> CreateMap(int xSize, int ySize, string name)
+
+        public async Task<Map> FindMap(string mapId)
+        {
+            var mapFilter = Builders<Map>.Filter.Eq(m => m.id, mapId);
+            return await _mapCollection.Find(mapFilter).FirstAsync();
+        }
+        public async Task<Map> CreateMap(int size, string name)
         {
             Map map = new Map()
             {
                 tiles = new List<List<MapTile>>(),
                 id = name
             };
-            for (int x = 0; x < xSize; x++)
+            for (int x = 0; x < size; x++)
             {
-                for (int y = 0; y < ySize; y++)
+                for (int y = 0; y < size; y++)
                 {
                     map.tiles[x][y] = new MapTile();
                 }
@@ -39,8 +45,20 @@ namespace Project
         }
         public async Task<Player> CreatePlayer(string mapId, Player player)
         {
-            var filter = Builders<Map>.Filter.Eq(m => m.id, mapId);
-            await _mapCollection.FindOneAndUpdateAsync(scoreFilter, update);
+            Map map = await FindMap(mapId);
+            Random rnd = new Random();
+            int randomX = rnd.Next(0, map.tiles.Count());
+            int randomY = rnd.Next(0, map.tiles.Count());
+
+            var filter = Builders<Map>.Filter.Eq(m => m.tiles[randomX][randomY].entity, null);
+            if (filter == null)
+            {
+                throw new NotImplementedException();
+            }
+            var update = Builders<Map>.Update.Set(m => m.tiles[randomX][randomY].entity, player);
+
+            await _mapCollection.UpdateOneAsync(filter, update);
+            return player;
         }
 
     }
